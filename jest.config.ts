3c6292f5f75da1c5146a9882f0f8c45ef47e12f8
JonathanAmarall <jhouamaral95@gmail.com>
2021-07-12 20:42:27 -0300

    feat: ensure SignUpController returns 400 if no name is provider

--- jest.config.ts
+++ jest.config.ts
@@ -1,7 +1,8 @@
 export default {
   roots: ['<rootDir>/src'],
-  collectCoverageFrom: ['<rootDir>/src/**/*.ts'],
+  collectCoverage: true,
   coverageDirectory: 'coverage',
+  coverageProvider: 'babel',
   testEnvironment: 'node',
   transform: {
     '.+\\.ts$': 'ts-jest',

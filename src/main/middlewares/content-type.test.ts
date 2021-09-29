28f0f1f2a86bfa2b5ce5adc7589f4c234deed8a7
JonathanAmarall <jhouamaral95@gmail.com>
2021-09-28 22:31:39 -0300

    feat: ensure request return json as default content type

---
+++ src/main/middlewares/content-type.test.ts
@@ -0,0 +1,21 @@
+import app from '../config/app';
+import request from 'supertest';
+
+describe('Content Type middleware', () => {
+  test('should return default Content-Type as json', async () => {
+    app.get('/test_content_type', (req, res) => {
+      res.send('');
+    });
+    await request(app).get('/test_content_type').expect('content-type', /json/);
+  });
+
+  test('should return Xml  Content-Type when forced', async () => {
+    app.get('/test_content_type_xml', (req, res) => {
+      res.type('xml');
+      res.send('');
+    });
+    await request(app)
+      .get('/test_content_type_xml')
+      .expect('content-type', /xml/);
+  });
+});

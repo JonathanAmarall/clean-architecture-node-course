57fd7a5eb9224811493c9e6800ba3e827230ee1d
JonathanAmarall <jhouamaral95@gmail.com>
2021-09-28 21:39:56 -0300

    refactor: clear comments

--- src/infra/criptografy/db/mongodb/account-repository/account.ts
+++ src/infra/criptografy/db/mongodb/account-repository/account.ts
@@ -2,14 +2,11 @@ import { MongoHelper } from './helpers/mongo-helper';
 import { AccountModel } from '../../../../../domain/models/account-model';
 import { AddAccountModel } from '../../../../../domain/usecases/add-account';
 import { AddAccountRepository } from './../../../../../data/protocols/add-account-repository';
-import { assert } from 'console';
 
 export class AccountMongoRepository implements AddAccountRepository {
   async add(accountData: AddAccountModel): Promise<AccountModel> {
     const accountCollection = MongoHelper.getCollection('accounts');
     const result = await accountCollection.insertOne(accountData);
-    console.log(result.insertedId.toString());
-    //const account = result.ops[0];
     return Object.assign({}, accountData, { id: result.insertedId.toString() });
   }
 }

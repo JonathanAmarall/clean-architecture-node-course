9cfba589136f904e02d4fd092a9be3bca65300c9
JonathanAmarall <jhouamaral95@gmail.com>
2021-10-15 20:58:26 -0300

    refactor: connect to mongo db before ingration tests

--- src/main/routes/signup-routes.test.ts
+++ src/main/routes/signup-routes.test.ts
@@ -1,7 +1,20 @@
+import { MongoHelper } from './../../infra/criptografy/db/mongodb/account-repository/helpers/mongo-helper';
 import app from '../config/app';
 import request from 'supertest';
 
 describe('SignUp Routes', () => {
+  beforeAll(async () => {
+    await MongoHelper.connect(process.env.MONGO_URL);
+  });
+
+  afterAll(async () => {
+    await MongoHelper.disconnect();
+  });
+
+  beforeEach(async () => {
+    const accountCollecion = MongoHelper.getCollection('accounts');
+    await accountCollecion.deleteMany({});
+  });
   test('should return an account on success', async () => {
     await request(app)
       .post('/api/signup')
